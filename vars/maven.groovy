e4ea29422f1243ee8430141dfaed4e0bbb388c98
Apips1 <afifngrh@gmail.com>
2025-07-02 14:33:36 +0700

    add List Parameter

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,20 +1,5 @@
-def call(String command) {
-    if (isUnix()) {
-        // Untuk sistem Unix/Linux
-        if (fileExists('./mvnw')) {
-            sh("chmod +x ./mvnw")
-            sh("./mvnw ${command}")
-        } else {
-            echo "Maven wrapper tidak ditemukan, menggunakan maven sistem"
-            sh("mvn ${command}")
-        }
-    } else {
-        // Untuk sistem Windows
-        if (fileExists('mvnw.cmd')) {
-            bat("mvnw.cmd ${command}")
-        } else {
-            echo "Maven wrapper tidak ditemukan, menggunakan maven sistem"
-            bat("mvn ${command}")
-        }
+def call(List commands) {
+    for (command in commands) {
+        bat "call mvnw.cmd ${command}"
     }
 }
