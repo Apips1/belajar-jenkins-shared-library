cf3ba3397b8e6abc53f518e3c9467672a6162ba0
Apips1 <afifngrh@gmail.com>
2025-07-02 14:17:33 +0700

    update

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,3 +1,20 @@
-def call(String command){
-    bat("mvnw.cmd ${command}")
+def call(String command) {
+    if (isUnix()) {
+        // Untuk sistem Unix/Linux
+        if (fileExists('./mvnw')) {
+            sh("chmod +x ./mvnw")
+            sh("./mvnw ${command}")
+        } else {
+            echo "Maven wrapper tidak ditemukan, menggunakan maven sistem"
+            sh("mvn ${command}")
+        }
+    } else {
+        // Untuk sistem Windows
+        if (fileExists('mvnw.cmd')) {
+            bat("mvnw.cmd ${command}")
+        } else {
+            echo "Maven wrapper tidak ditemukan, menggunakan maven sistem"
+            bat("mvn ${command}")
+        }
+    }
 }
